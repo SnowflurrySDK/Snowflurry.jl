567ef088f8af7abe293cd1acb13bb4fbcb58f35b
Nicolas Poirier <nicolas@anyonsys.com>
2022-06-14 14:46:26 -0400

    feat(visualize): add plot_unit_sphere
    Added BlochSphere struct which required adding the Parameters package to enable default values and keyword constructors.
    
    Issue #17

--- src/core/visualize.jl
+++ src/core/visualize.jl
@@ -1,4 +1,5 @@
-# using PyPlot
+using Parameters
+
 function plot_histogram(circuit::QuantumCircuit, shots_count::Int)
     data = simulate_shots(circuit, shots_count)
     datamap = proportionmap(data)
@@ -18,4 +19,27 @@ end
 
 function viz_wigner(ρ, x, y)
     return contour(x, y, (x,y) -> wigner(ρ, x, y), fill = true)
+end
+
+@with_kw struct BlochSphere
+    num_points_per_horizontal_circle::Int = 50
+    sphere_color = "#FFEEDD"
+    sphere_blending_alpha = 0.4
+end
+
+function plot_unit_sphere(bloch_sphere = BlochSphere())
+    plotly()
+    (x, y, z) = get_unit_sphere_coordinates(bloch_sphere.num_points_per_horizontal_circle)
+    surface(x, y, z, linewidth=0, color=bloch_sphere.sphere_color,
+        alpha=bloch_sphere.sphere_blending_alpha)
+end
+
+
+function get_unit_sphere_coordinates(num_points_per_horizontal_circle)
+    inclination = range(0, stop=π, length=num_points_per_horizontal_circle)
+    azimuth = range(0, stop=2*π, length=num_points_per_horizontal_circle)
+    x = sin.(inclination) * sin.(azimuth)'
+    y = sin.(inclination) * cos.(azimuth)'
+    z = cos.(inclination) * ones(num_points_per_horizontal_circle)'
+    return (x, y, z)
 end
