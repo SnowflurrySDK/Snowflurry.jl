01ee40ffce64f13daf6bc2aee1f9e4eb800efe21
Nicolas Poirier <nicolas@anyonsys.com>
2022-06-16 00:42:01 -0400

    feat(visualize): replace Plots by PlotlyJS for bloch sphere
    Plots.jl cannot add 3D annotations to plots, so it was replace by PlotlyJS.jl.
    
    Issue #17

--- src/core/visualize.jl
+++ src/core/visualize.jl
@@ -1,4 +1,5 @@
 using Parameters
+using PlotlyJS
 
 function plot_histogram(circuit::QuantumCircuit, shots_count::Int)
     data = simulate_shots(circuit, shots_count)
@@ -40,15 +41,14 @@ end
 end
 
 function plot_bloch_sphere(bloch_sphere = BlochSphere())
-    plotlyjs()
     plot_unit_sphere(bloch_sphere)
 end
 
 function plot_unit_sphere(bloch_sphere)
-    plot_unit_sphere_surface(bloch_sphere)
-    plot_vertical_circular_wires(bloch_sphere)
-    plot_horizontal_circular_wires(bloch_sphere)
-    plot_axes_lines(bloch_sphere)
+    plot = plot_unit_sphere_surface(bloch_sphere)
+    plot = plot_vertical_circular_wires(bloch_sphere, plot)
+    plot = plot_horizontal_circular_wires(bloch_sphere, plot)
+    plot = plot_axes_lines(bloch_sphere, plot)
 end
 
 function plot_unit_sphere_surface(bloch_sphere)
@@ -57,17 +57,41 @@ function plot_unit_sphere_surface(bloch_sphere)
     x = sin.(inclination) * cos.(azimuth)'
     y = sin.(inclination) * sin.(azimuth)'
     z = cos.(inclination) * ones(bloch_sphere.num_points_per_line)'
-    surface(x, y, z,
-        color=bloch_sphere.sphere_color,
-        alpha=bloch_sphere.sphere_blending_alpha,
-        showaxis=false,
-        size=(bloch_sphere.window_width, bloch_sphere.window_height),
-        colorbar=false,
-        framestyle=:none)
-    return (x, y, z)
+    color_scale = [[0, bloch_sphere.sphere_color],
+        [1, bloch_sphere.sphere_color]]
+    surface_color = zeros(size(z))
+    sphere_surface = PlotlyJS.surface(x=x, y=y, z=z,
+        surfacecolor=surface_color,
+        colorscale=color_scale,
+        cmin=0,
+        cmax=1,
+        opacity=bloch_sphere.sphere_blending_alpha,
+        showscale=false)
+    layout = Layout(width=bloch_sphere.window_width,
+        height=bloch_sphere.window_height,
+        autosize=false,
+        scene=attr(xaxis=attr(showbackground=false,
+                showaxeslabels=false,
+                showline=false,
+                showspikes=false,
+                showticklabels=false,
+                title=attr(text="")),
+            yaxis=attr(showbackground=false,
+                showaxeslabels=false,
+                showline=false,
+                showspikes=false,
+                showticklabels=false,
+                title=attr(text="")),
+            zaxis=attr(showbackground=false,
+                showaxeslabels=false,
+                showline=false,
+                showspikes=false,
+                showticklabels=false,
+                title=attr(text=""))))
+        return PlotlyJS.Plot(sphere_surface, layout)
 end
 
-function plot_vertical_circular_wires(bloch_sphere)
+function plot_vertical_circular_wires(bloch_sphere, plot)
     inclination = range(0, stop=π, length=bloch_sphere.num_points_per_line)
     num_semi_circles = 2*bloch_sphere.num_vertical_wires
     for i in 1:num_semi_circles
@@ -75,15 +99,18 @@ function plot_vertical_circular_wires(bloch_sphere)
         x = sin.(inclination)*cos(azimuth)
         y = sin.(inclination)*sin(azimuth)
         z = cos.(inclination)
-        path3d!(x, y, z,
-            color=bloch_sphere.wire_color,
-            alpha=bloch_sphere.wire_blending_alpha,
-            linewidth=bloch_sphere.wires_width,
-            legend=false)
+        trace = PlotlyJS.scatter3d(x=x, y=y, z=z,
+            mode="lines",
+            opacity=bloch_sphere.wire_blending_alpha,
+            line=attr(color=bloch_sphere.wire_color,
+                width=bloch_sphere.wires_width),
+            showlegend=false)
+        push!(plot.data, trace)
     end
+    return plot
 end
 
-function plot_horizontal_circular_wires(bloch_sphere)
+function plot_horizontal_circular_wires(bloch_sphere, plot)
     azimuth = range(0, stop=2*π, length=bloch_sphere.num_points_per_line)
     num_circles = bloch_sphere.num_horizontal_wires
     for i in 1:num_circles
@@ -91,30 +118,38 @@ function plot_horizontal_circular_wires(bloch_sphere)
         x = sin(inclination)*cos.(azimuth)
         y = sin(inclination)*sin.(azimuth)
         z = cos(inclination)*ones(length(azimuth))
-        path3d!(x, y, z,
-            color=bloch_sphere.wire_color,
-            alpha=bloch_sphere.wire_blending_alpha,
-            linewidth=bloch_sphere.wires_width,
-            legend=false)
+        trace = PlotlyJS.scatter3d(x=x, y=y, z=z,
+            mode="lines",
+            opacity=bloch_sphere.wire_blending_alpha,
+            line=attr(color=bloch_sphere.wire_color,
+                width=bloch_sphere.wires_width),
+            showlegend=false)
+        push!(plot.data, trace)
     end
+    return plot
 end
 
-function plot_axes_lines(bloch_sphere)
+function plot_axes_lines(bloch_sphere, plot)
     axes_points = range(-1, 1, length=bloch_sphere.num_points_per_line)
     zeros_points = zeros(bloch_sphere.num_points_per_line)
-    path3d!(axes_points, zeros_points, zeros_points,
-        color=bloch_sphere.axes_color,
-        alpha=bloch_sphere.axes_blending_alpha,
-        linewidth=bloch_sphere.axes_line_width,
-        legend=false)
-    path3d!(zeros_points, axes_points, zeros_points,
-        color=bloch_sphere.axes_color,
-        alpha=bloch_sphere.axes_blending_alpha,
-        linewidth=bloch_sphere.axes_line_width,
-        legend=false)
-    path3d!(zeros_points, zeros_points, axes_points,
-        color=bloch_sphere.axes_color,
-        alpha=bloch_sphere.axes_blending_alpha,
-        linewidth=bloch_sphere.axes_line_width,
-        legend=false)
+    x_trace = PlotlyJS.scatter3d(x=axes_points, y=zeros_points, z=zeros_points,
+        mode="lines",
+        opacity=bloch_sphere.axes_blending_alpha,
+        line=attr(color=bloch_sphere.axes_color,
+        width=bloch_sphere.axes_line_width),
+        showlegend=false)
+    y_trace = PlotlyJS.scatter3d(x=zeros_points, y=axes_points, z=zeros_points,
+        mode="lines",
+        opacity=bloch_sphere.axes_blending_alpha,
+        line=attr(color=bloch_sphere.axes_color,
+        width=bloch_sphere.axes_line_width),
+        showlegend=false)
+    z_trace = PlotlyJS.scatter3d(x=zeros_points, y=zeros_points, z=axes_points,
+        mode="lines",
+        opacity=bloch_sphere.axes_blending_alpha,
+        line=attr(color=bloch_sphere.axes_color,
+        width=bloch_sphere.axes_line_width),
+        showlegend=false)
+    push!(plot.data, x_trace, y_trace, z_trace)
+    return plot
 end
