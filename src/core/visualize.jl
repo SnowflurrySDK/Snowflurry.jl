312e4f1b8ecfde0cf4d807b31c341dc28decf241
Nicolas Poirier <nicolas@anyonsys.com>
2022-06-20 11:37:02 -0400

    feat(visualize): add plot_bloch_sphere for circuit
    Issue #17

--- src/core/visualize.jl
+++ src/core/visualize.jl
@@ -45,6 +45,14 @@ end
     show_qubit_id = true
 end
 
+function plot_bloch_sphere(circuit::QuantumCircuit;
+    qubit_id::Int = 1,
+    bloch_sphere::BlochSphere = BlochSphere())
+    
+    ket = simulate(circuit)
+    return plot_bloch_sphere(ket2dm(ket), qubit_id=qubit_id, bloch_sphere=bloch_sphere)
+end
+
 function plot_bloch_sphere(ket::Ket;
     qubit_id::Int = 1,
     bloch_sphere::BlochSphere = BlochSphere())
