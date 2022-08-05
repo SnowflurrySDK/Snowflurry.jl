129d73624d32752b28b44c6cfefe095338f0da73
Nicolas Poirier <nicolas@anyonsys.com>
2022-08-04 21:05:45 -0400

    feat(randomized_benchmarking): add get_sequence_fidelities
    Issue #29

--- src/benchmarking/randomized_benchmarking.jl
+++ src/benchmarking/randomized_benchmarking.jl
@@ -1,27 +1,59 @@
 @with_kw struct RandomizedBenchmarkingProperties
-    num_qubits_on_device::Int; @assert num_qubits_on_device>0
-    num_bits_on_device::Int = 0; @assert num_bits_on_device>=0
+    num_qubits_on_device::Int; @assert num_qubits_on_device > 0
+    num_bits_on_device::Int = 0; @assert num_bits_on_device >= 0
     target_qubits::Array{Int} = 1:num_qubits_on_device
+    num_shots_per_circuit::Int = 100; @assert num_shots_per_circuit > 0
     sequence_length_list::Array{Int}; @assert all(x->(x>0), sequence_length_list)
-    num_sequences_per_length::Array{Int} = 100*ones(Int, length(sequence_length_list));
-        @assert all(x->(x>0), num_sequences_per_length)
-        @assert length(sequence_length_list) == length(num_sequences_per_length)
+    num_circuits_per_length::Array{Int} = 100*ones(Int, length(sequence_length_list));
+        @assert all(x->(x>0), num_circuits_per_length)
+        @assert length(sequence_length_list) == length(num_circuits_per_length)
 end
 
-function run_randomized_benchmarking(circuit_simulator,
+function run_randomized_benchmarking(simulate_shots, transpile!,
     properties::RandomizedBenchmarkingProperties)
     
-    for sequence_length in properties.sequence_length_list
-        for i in 1:properties.num_sequences_per_length
-
-        end
-    end
+    sequence_fidelities = get_sequence_fidelities(simulate_shots, transpile!,
+        properties)
 end
 
-function get_survival_probability(circuit_simulator, sequence_length,
+function get_sequence_fidelities(simulate_shots, transpile!,
     properties::RandomizedBenchmarkingProperties)
 
+    circuit_list = QuantumCircuit[]
+    for i_length in 1:length(properties.sequence_length_list)
+        circuit_list_for_length =
+            get_random_clifford_circuits(i_length, transpile!, properties)
+        append!(circuit_list, circuit_list_for_length)
+    end
+    
+    shots_list = simulate_shots(circuit_list, properties.num_shots_per_circuit)
+    sequence_fidelity_list = []
+    circuit_id = 1
+    for i_length in 1:length(properties.sequence_length_list)
+        survival_probability_sum = 0
+        num_circuits_for_length = properties.num_circuits_per_length[i_length]
+        for j_circuit_for_length = 1:num_circuits_for_length
+            survival_probability_sum += get_survival_probability(shots_list[circuit_id],
+                properties.target_qubits)
+            circuit_id += 1
+        end
+        sequence_fidelity = survival_probability_sum/num_circuits_for_length
+        push!(sequence_fidelity_list, sequence_fidelity)
+    end
+    return sequence_fidelity_list
+end
 
+function get_survival_probability(shots, target_qubits)
+    num_successes = 0
+    num_target_qubits = length(target_qubits)
+    for measurement in shots
+        trimmed_measurement = measurement[target_qubits]
+        if trimmed_measurement == "0"^num_target_qubits
+            num_successes += 1
+        end
+    end
+    survival_probability = num_successes/length(shots)
+    return survival_probability
 end
 
 function get_random_clifford_circuits(sequence_length_id, transpile!,
@@ -31,7 +63,7 @@ function get_random_clifford_circuits(sequence_length_id, transpile!,
     num_target_qubits = length(properties.target_qubits)
     qubit_map = Dict(zip(1:num_target_qubits, properties.target_qubits))
 
-    for i_sequence in 1:properties.num_sequences_per_length[sequence_length_id]
+    for i_circuit in 1:properties.num_circuits_per_length[sequence_length_id]
         old_clifford = get_random_clifford(num_target_qubits)
         circuit = get_transpiled_circuit(old_clifford, properties, qubit_map, transpile!)
         clifford_product = old_clifford
--- test/randomized_benchmarking.jl
+++ test/randomized_benchmarking.jl
@@ -3,9 +3,10 @@ using Test
 
 @testset "initial_benchmark_test" begin
     properties = RandomizedBenchmarkingProperties(num_qubits_on_device=3,
-        target_qubits=[2,3], sequence_length_list=[2], num_sequences_per_length=[1])
+        target_qubits=[2,3], sequence_length_list=[2, 4], num_circuits_per_length=[2, 2])
     transpile!(x) = x
-    circuits = Snowflake.get_random_clifford_circuits(1, transpile!, properties)
-    shots = simulate_shots(circuits[1], 5)
-    @test all(x->(x=="000"), shots)
+
+    sequence_fidelities = Snowflake.get_sequence_fidelities(simulate_shots, transpile!, 
+        properties)
+    @test sequence_fidelities == [1.0, 1.0]
 end
