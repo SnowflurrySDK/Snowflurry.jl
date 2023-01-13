6cfa6581e166f7c7aab1b4a2f0895ecdba8572ba
Nicolas Poirier <nicolas@anyonsys.com>
2023-01-13 15:05:35 -0500

    test(qpu): correct does_circuit_satisfy_qpu_connectivity
    Issue #47

--- test/qpu.jl
+++ test/qpu.jl
@@ -9,13 +9,14 @@ using Test
     
     c = QuantumCircuit(qubit_count = 3, bit_count = 0)
     push_gate!(c, sigma_x(1))
-    push_gate!(c, control_z(1,2))
+    push_gate!(c, control_z(2,1))
 
     is_circuit_ok,  = is_circuit_native_on_qpu(c, qpu)
     @test is_circuit_ok
 
-    push_gate!(c,control_z(1,3))
+    push_gate!(c,control_z(2,3))
     is_circuit_ok, = does_circuit_satisfy_qpu_connectivity(c, qpu)
+    println(does_circuit_satisfy_qpu_connectivity(c, qpu))
     @test is_circuit_ok
 end
 
