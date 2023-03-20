02ea9cb468d66810d510819a7c3eb8f8846ae403
Facundo Sosa-Rey <facundo.sosarey@gmail.com>
2023-03-20 14:39:37 -0400

    docs: comments

--- benchmarking/runBenchmarks_all.jl
+++ benchmarking/runBenchmarks_all.jl
@@ -1,5 +1,6 @@
 using Dates
 
+# this timestamp will propagate to all benchmarks output files
 time_stamp=Dates.format(now(),"dd-mm-YYYY_HH:MM")
 
 for file in readlines(joinpath(@__DIR__, "benchmarkList"))
