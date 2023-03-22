40c16f632b7f858db61515d71c4361113827e26e
Nicolas Poirier <nicolas@anyonsys.com>
2023-03-22 15:04:28 -0400

    fix(ci): remove deploydocs for pull request ci
    Issue #62

---
+++ docs/make_and_deploy.jl
@@ -0,0 +1,8 @@
+include("make.jl")
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+deploydocs(
+    repo = "github.com/anyonlabs/Snowflake.jl.git",
+)
