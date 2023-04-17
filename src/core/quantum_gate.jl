42f9c590be08e435948c8a61a26eee7ae74575d2
Paul le Roux <pleroux@anyonsys.com>
2023-04-17 11:48:21 -0400

    refactor: fall back to generic implementation of gate inverse

--- src/core/quantum_gate.jl
+++ src/core/quantum_gate.jl
@@ -1182,8 +1182,6 @@ end
 
 get_operator(::Swap, T::Type{<:Complex}=ComplexF64) = swap(T)
 
-Base.inv(gate::Swap) = swap(gate.target_1,gate.target_2)
-
 get_connected_qubits(gate::Swap)=[gate.target_1, gate.target_2]
 
 """
