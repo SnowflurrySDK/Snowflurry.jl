354b43b9ea28cb479ca7fa3c43fe7118080f8604
Facundo Sosa-Rey <facundo.sosarey@gmail.com>
2023-04-19 14:17:57 -0400

    remove unused file

--- test/regression.jl
+++
@@ -1,6 +0,0 @@
-using Snowflake
-using Test
-
-@testset "regression test: gate with qubit out of range of circuit should fail" begin
-    @test_throws DomainError QuantumCircuit(qubit_count=1, gates=[sigma_x(5)])
-end
