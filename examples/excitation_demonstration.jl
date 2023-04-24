f2cdf98e45bbf4f7670a3accb117a890ba77c165
Paul le Roux <pleroux0@outlook.com>
2023-04-23 23:40:15 -0400

    docs: add excitation demonstration example code

---
+++ examples/excitation_demonstration.jl
@@ -0,0 +1,17 @@
+using Snowflake
+
+circuit = QuantumCircuit(qubit_count = 1)
+
+push!(circuit, sigma_x(1))
+
+user = ENV["ANYON_QUANTUM_USER"]
+token = ENV["ANYON_QUANTUM_TOKEN"]
+host = ENV["ANYON_QUANTUM_HOST"]
+
+client = Client(host=host, user=user, access_token=token)
+qpu = AnyonQPU(client)
+
+num_repititions = 200
+result = run_job(qpu, circuit, num_repititions)
+
+println(result)
