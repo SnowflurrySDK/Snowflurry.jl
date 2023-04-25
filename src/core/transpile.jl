e205d53b10c141be8a2ea7b1c55dc43afdd7a76c
Nicolas Poirier <nicolas@anyonsys.com>
2023-04-25 11:44:13 -0400

    docs(transpile): add RemoveSwapBySwappingGates
    Issue #124

--- src/core/transpile.jl
+++ src/core/transpile.jl
@@ -1502,6 +1502,45 @@ transpile(::TrivialTranspiler, circuit::QuantumCircuit)::QuantumCircuit=circuit
 
 struct RemoveSwapBySwappingGates <: Transpiler end
 
+"""
+    transpile(::RemoveSwapBySwappingGates, circuit::QuantumCircuit)::QuantumCircuit
+
+Removes the `Swap` gates from the `circuit` assuming all-to-all connectivity.
+
+!!! warning "The initial state must be the ground state!"
+    This transpiler stage assumes that the input state is ``|0\\rangle^{\\otimes N}``
+    where ``N`` is the number of qubits. The stage should not be used on sub-circuits
+    where the input state is not ``|0\\rangle^{\\otimes N}``.
+
+This transpiler stage eliminates `Swap` gates by reordering the gates preceding each `Swap`
+gate.
+
+# Examples
+```jldoctest
+julia> transpiler = RemoveSwapBySwappingGates();
+
+julia> circuit = QuantumCircuit(qubit_count=2, gates=[hadamard(1), swap(1,2), sigma_x(2)])
+Quantum Circuit Object:
+   qubit_count: 2 
+q[1]:──H────☒───────
+            |       
+q[2]:───────☒────X──
+                    
+
+
+
+julia> transpiled_circuit = transpile(transpiler, circuit)
+Quantum Circuit Object:
+   qubit_count: 2 
+q[1]:──────────
+               
+q[2]:──H────X──
+               
+
+
+
+```
+"""
 function transpile(::RemoveSwapBySwappingGates, circuit::QuantumCircuit)::QuantumCircuit
     gates = get_circuit_gates(circuit)
     qubit_count = get_num_qubits(circuit)
