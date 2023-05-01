aadb59d3df39d5c7e61c5589467796391f3f17f9
Nicolas Poirier <nicolas@anyonsys.com>
2023-05-01 10:36:05 -0400

    refactor(quantum_circuit): use slurping to push gates
    Issue #200

--- src/core/quantum_circuit.jl
+++ src/core/quantum_circuit.jl
@@ -27,7 +27,7 @@ Base.@kwdef struct QuantumCircuit
         c=new(qubit_count,[])
 
         # add gates, with ensure_gate_is_in_circuit()
-        push!(c,gates)
+        push!(c,gates...)
 
         return c
     end
@@ -69,15 +69,10 @@ q[2]:───────X────X──
 
 ```
 """
-function Base.push!(circuit::QuantumCircuit, gate::AbstractGate)
-    ensure_gate_is_in_circuit(circuit, gate)
-    push!(get_circuit_gates(circuit), gate)
-    return circuit
-end
-
-function Base.push!(circuit::QuantumCircuit, gates::Vector{<:AbstractGate})
-    for gate in gates
-        push!(circuit, gate)
+function Base.push!(circuit::QuantumCircuit, gates::AbstractGate...)
+    for single_gate in gates
+        ensure_gate_is_in_circuit(circuit, single_gate)
+        push!(get_circuit_gates(circuit), single_gate)
     end
     return circuit
 end
