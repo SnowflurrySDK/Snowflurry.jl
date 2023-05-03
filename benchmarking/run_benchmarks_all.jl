7847a7864b31e51cf4d84d2972fdbd932cfc61ec
Facundo Sosa-Rey <facundo.sosarey@gmail.com>
2023-05-03 16:59:35 -0400

    rename: using snake_convention

--- benchmarking/run_benchmarks_all.jl
+++ benchmarking/run_benchmarks_all.jl
@@ -3,6 +3,6 @@ using Dates
 # this timestamp will propagate to all benchmarks output files
 time_stamp=Dates.format(now(),"dd-mm-YYYY_HHhMM")
 
-for file in readlines(joinpath(@__DIR__, "benchmarkList"))
+for file in readlines(joinpath(@__DIR__, "benchmark_list_all"))
     include(file * ".jl")
 end
--- benchmarking/run_benchmarks_single_target.jl
+++ benchmarking/run_benchmarks_single_target.jl
@@ -2,6 +2,6 @@ using Dates
 
 time_stamp=Dates.format(now(),"dd-mm-YYYY_HHhMM")
 
-for file in readlines(joinpath(@__DIR__, "benchmarkList_singleTarget"))
+for file in readlines(joinpath(@__DIR__, "benchmark_list_single_target"))
     include(file * ".jl")
 end
