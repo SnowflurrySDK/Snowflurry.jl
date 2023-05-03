edf91133d522575cc4731b872e63b3ec19ec3fe4
Facundo Sosa-Rey <facundo.sosarey@gmail.com>
2023-05-03 17:31:09 -0400

    docs: correct comments in .sh script

--- benchmarking/configure_benchmarks.sh
+++ benchmarking/configure_benchmarks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script configures the Julia Project to use the 
 # Snowflake Package located in the current path (pwd), 
-# in development mode, then builds the documentation.
+# in development mode, in order to run the benchmarks on it.
 julia --project=benchmarking -e 'using Pkg;
           Pkg.develop(PackageSpec(path=pwd())); 
           Pkg.instantiate();'
