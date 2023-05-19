3cac54a1d542f86ec9033b2b1c440dba0632c2eb
Paul le Roux <pleroux@anyonsys.com>
2023-05-19 01:13:28 -0400

    test: clean up master equation relaxtion test

--- test/dynamic_system.jl
+++ test/dynamic_system.jl
@@ -21,28 +21,26 @@ end
     @test typeof(ψ)==Vector{Ket{ComplexF32}}
 end
 
-@testset "Master Equation: relaxation" begin
-    ψ_0 = spin_up()
-    ω = 0.0*pi #Rabi frequency
-    H = ω/2.0*sigma_x()
-    T = 2.0
-    t = 0.0:0.001:T
-    #master equation solver
-    projection = ψ_0*ψ_0'
-    Γ = 0.05  #relaxation rate
-    prob = mesolve(H, ket2dm(ψ_0), t, c_ops=[sqrt(Γ)*sigma_m()], e_ops=[projection])
-    @test last(prob) ≈ exp(-Γ*T) atol=1.e-4
-end
+@testset "Master Equation" begin
+    @testset "relaxation" begin
+        function test_relaxation(dtype, Γ, t)
+            Ψ_0 = spin_up(dtype)
+            H = dtype(0)*sigma_x(dtype)
+            projection = Ψ_0*Ψ_0'
+            prob = mesolve(
+                H,
+                ket2dm(Ψ_0),
+                t,
+                c_ops=[sqrt(dtype(Γ))*sigma_m(dtype)],
+                e_ops=[projection]
+            )
 
-@testset "Master Equation: relaxation ComplexF32" begin
-    ψ_0 = spin_up(ComplexF32)
-    ω = 0.0*pi #Rabi frequency
-    H = ComplexF32(ω/2.0)*sigma_x(ComplexF32)
-    T = 2.0
-    t = 0.0:0.001:T
-    #master equation solver
-    projection = ψ_0*ψ_0'
-    Γ = 0.05  #relaxation rate
-    prob = mesolve(H, ket2dm(ψ_0), t, c_ops=[sqrt(Γ)*sigma_m()], e_ops=[projection])
-    @test last(prob) ≈ exp(-Γ*T) atol=1.e-4
+            @test prob ≈ exp.(-Γ*collect(t)) atol=1.e-4
+        end
+
+        t = 0.0:0.01:2.0
+        Γ = 0.5
+        test_relaxation(ComplexF64, Γ, t)
+        test_relaxation(ComplexF32, Γ, t)
+    end
 end
