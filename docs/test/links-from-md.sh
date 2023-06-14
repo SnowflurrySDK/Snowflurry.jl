fed5f39bcf750dbfe52960f172c01d78197e129d
Thomas McKee <caffeine.storm@gmail.com>
2023-06-14 16:20:02 -0400

    fix(tests): better filtering for links in markdown

--- docs/test/links-from-md.sh
+++ docs/test/links-from-md.sh
@@ -3,7 +3,7 @@
 # Reads a markdown file and lists the targets of all markdown links, one per
 # line
 
-sed '/^```/,/^```$/{d}' | # erase code blocks
+sed '/^\s*```/,/^```\s*$/{d}' | # erase code blocks
 sed 's,),)\n,g' | # add newlines after every close-paren so that downstream can assume <2 links per line
 grep '\[' | # select lines of the form [text](link)
 grep -v '\!\[' | # but skip lines like ![gh-replacement](link)
