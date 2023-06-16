cc066d9b0282fda5be4bbdb1716c42983845e92b
Thomas McKee <caffeine.storm@gmail.com>
2023-06-16 11:56:22 -0400

    feat: prefer 'shot_count' to 'num_repetitions'

--- tutorials/excitation_demonstration.jl
+++ tutorials/excitation_demonstration.jl
@@ -10,7 +10,7 @@ host = ENV["ANYON_QUANTUM_HOST"]
 
 qpu = AnyonYukonQPU(host=host, user=user, access_token=token)
 
-num_repetitions = 200
-result = run_job(qpu, circuit, num_repetitions)
+shot_count = 200
+result = run_job(qpu, circuit, shot_count)
 
 println(result)
