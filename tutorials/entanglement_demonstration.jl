3c983b43963f2f8d47fb5a61d4c06602804631c0
Alireza Najafi-Yazdi <alireza@anyonsys.com>
2023-12-05 15:36:28 -0500

    add @info to print the circuit_id once a circuit is submitted

--- tutorials/entanglement_demonstration.jl
+++ tutorials/entanglement_demonstration.jl
@@ -1,3 +1,4 @@
+using Dates
 using Snowflurry
 
 circuit = QuantumCircuit(qubit_count = 2)
@@ -5,16 +6,18 @@ circuit = QuantumCircuit(qubit_count = 2)
 push!(circuit, hadamard(1))
 push!(circuit, control_x(1, 2))
 
-user = ENV["ANYON_QUANTUM_USER"]
-token = ENV["ANYON_QUANTUM_TOKEN"]
-host = ENV["ANYON_QUANTUM_HOST"]
+user = ENV["ANYON_USER"]
+token = ENV["ANYON_TOKEN"]
+host = ENV["ANYON_QPU"]
 
 qpu = AnyonYukonQPU(host = host, user = user, access_token = token)
 
+shot_count = 200
+result_1 = transpile_and_run_job(qpu, circuit, shot_count)
+println(result_1)
+
+# alternarively, one can explicitly transpile a code and run the transpiled circuit. 
+
 transpiler = get_transpiler(qpu)
 transpiled_circuit = transpile(transpiler, circuit)
-
-shot_count = 200
-result = run_job(qpu, transpiled_circuit, shot_count)
-
-println(result)
+result_2 = run_job(qpu, transpiled_circuit, shot_count)
