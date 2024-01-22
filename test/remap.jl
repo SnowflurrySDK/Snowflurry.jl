0672eebf30d83e7922283e66bb398e1c08b78aab
Facundo Sosa-Rey <facundo@anyonsys.com>
2024-01-22 15:03:16 -0500

    chore: run formatter

--- test/remap.jl
+++ test/remap.jl
@@ -23,39 +23,64 @@ test_cases = [
     # (data_2qubits, Dict(2 => 2), Dict{String,Int}("00"=> 21,"10"=> 4300))
     # #
     # # single readout, with swap
-    (data_2qubits, Dict(1 => 2), Dict{String,Int}("0" =>21, "1" =>4300))
-    (data_2qubits, Dict(2 => 1), Dict{String,Int}("00"=>301, "10"=>4020))
-    (data_2qubits, Dict(3 => 1), Dict{String,Int}("000"=>301,"100"=> 4020)) #expanding Hilbert space
+    (data_2qubits, Dict(1 => 2), Dict{String,Int}("0" => 21, "1" => 4300))
+    (data_2qubits, Dict(2 => 1), Dict{String,Int}("00" => 301, "10" => 4020))
+    (data_2qubits, Dict(3 => 1), Dict{String,Int}("000" => 301, "100" => 4020)) #expanding Hilbert space
     # #
-    (data_2qubits, Dict(1 => 2, 2 => 1), Dict{String,Int}("00"=>1,"01"=> 300,"10"=> 20,"11"=>4000)) # symmetric swaps
+    (
+        data_2qubits,
+        Dict(1 => 2, 2 => 1),
+        Dict{String,Int}("00" => 1, "01" => 300, "10" => 20, "11" => 4000),
+    ) # symmetric swaps
     # #
     (data_3qubits, Dict(1 => 1, 2 => 2, 3 => 3), data_3qubits)
     (data_3qubits, Dict(3 => 3, 2 => 2, 1 => 1), data_3qubits) # out of order
     # #
     # # single readout, no swaps
-    (data_3qubits, Dict(1 => 1), Dict{String,Int}("0"=>7050301,"1"=>80604020))
-    (data_3qubits, Dict(2 => 2),  Dict{String,Int}("00"=>650021, "10"=>87004300))
-    (data_3qubits, Dict(3 => 3), Dict{String,Int}("000"=>4321, "100"=> 87650000))
+    (data_3qubits, Dict(1 => 1), Dict{String,Int}("0" => 7050301, "1" => 80604020))
+    (data_3qubits, Dict(2 => 2), Dict{String,Int}("00" => 650021, "10" => 87004300))
+    (data_3qubits, Dict(3 => 3), Dict{String,Int}("000" => 4321, "100" => 87650000))
     # #
     # # single readout, with swap
-    (data_3qubits, Dict(1 => 2),  Dict{String,Int}("0"=>650021,"1"=>87004300))
-    (data_3qubits, Dict(1 => 3), Dict{String,Int}("0"=>4321,"1"=>87650000))
-    (data_3qubits, Dict(2 => 3), Dict{String,Int}("00"=>4321,"10"=>87650000))
+    (data_3qubits, Dict(1 => 2), Dict{String,Int}("0" => 650021, "1" => 87004300))
+    (data_3qubits, Dict(1 => 3), Dict{String,Int}("0" => 4321, "1" => 87650000))
+    (data_3qubits, Dict(2 => 3), Dict{String,Int}("00" => 4321, "10" => 87650000))
     # #
-    (data_3qubits, Dict(2 => 1), Dict{String,Int}("00"=>7050301,"10"=> 80604020))
-    (data_3qubits, Dict(3 => 1), Dict{String,Int}("000"=>7050301, "100"=> 80604020))
-    (data_3qubits, Dict(3 => 2), Dict{String,Int}("000"=>650021, "100"=> 87004300))
+    (data_3qubits, Dict(2 => 1), Dict{String,Int}("00" => 7050301, "10" => 80604020))
+    (data_3qubits, Dict(3 => 1), Dict{String,Int}("000" => 7050301, "100" => 80604020))
+    (data_3qubits, Dict(3 => 2), Dict{String,Int}("000" => 650021, "100" => 87004300))
     # #
     # # multiple readouts, single swap
-    (data_3qubits, Dict(1 => 2, 3 => 3), Dict{String,Int}("000"=>21, "001"=>4300, "100"=>  650000, "101"=>  87000000))
-    (data_3qubits, Dict(2 => 3, 1 => 1), Dict{String,Int}("00"=>301, "01"=>4020, "10"=>7050000, "11"=> 80600000))
+    (
+        data_3qubits,
+        Dict(1 => 2, 3 => 3),
+        Dict{String,Int}("000" => 21, "001" => 4300, "100" => 650000, "101" => 87000000),
+    )
+    (
+        data_3qubits,
+        Dict(2 => 3, 1 => 1),
+        Dict{String,Int}("00" => 301, "01" => 4020, "10" => 7050000, "11" => 80600000),
+    )
     # # #
     # # multiple readouts, multiple swap
-    (data_3qubits, Dict(1 => 2, 2 => 3), Dict{String,Int}("00"=>21, "01"=>4300, "10"=>650000, "11"=> 87000000))
+    (
+        data_3qubits,
+        Dict(1 => 2, 2 => 3),
+        Dict{String,Int}("00" => 21, "01" => 4300, "10" => 650000, "11" => 87000000),
+    )
     (
         data_3qubits,
         Dict(1 => 3, 2 => 2, 3 => 1),
-        Dict{String,Int}("000"=>1,"001"=> 50000,"010"=> 300,"011"=> 7000000,"100"=> 20,"101"=> 600000,"110"=> 4000,"111"=>80000000),
+        Dict{String,Int}(
+            "000" => 1,
+            "001" => 50000,
+            "010" => 300,
+            "011" => 7000000,
+            "100" => 20,
+            "101" => 600000,
+            "110" => 4000,
+            "111" => 80000000,
+        ),
     )
 ]
 
