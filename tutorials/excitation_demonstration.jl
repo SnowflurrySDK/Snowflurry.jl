29b36c53ed0221800b90d093bc4d88d3db2fb939
Alireza Najafi-Yazdi <alireza@anyonsys.com>
2024-01-25 14:12:34 -0500

    fix: thunderhead-dev

--- tutorials/excitation_demonstration.jl
+++ tutorials/excitation_demonstration.jl
@@ -6,9 +6,10 @@ push!(circuit, sigma_x(1))
 push!(circuit, readout(1, 1))
 
 user = ENV["THUNDERHEAD_USER"]
-token = ENV["THUNDERHEAD_TOKEN"]
+token = ENV["THUNDERHEAD_API_TOKEN"]
 host = ENV["THUNDERHEAD_HOST"]
 project_id = ENV["THUNDERHEAD_PROJECT_ID"]
+
 
 qpu = AnyonYukonQPU(host = host, user = user, access_token = token, project_id = project_id)
 
--- tutorials/get_qpu_metadata.jl
+++ tutorials/get_qpu_metadata.jl
@@ -1,9 +1,10 @@
 using Snowflurry
 
 user = ENV["THUNDERHEAD_USER"]
-token = ENV["THUNDERHEAD_TOKEN"]
+token = ENV["THUNDERHEAD_API_TOKEN"]
 host = ENV["THUNDERHEAD_HOST"]
 project_id = ENV["THUNDERHEAD_PROJECT_ID"]
+
 
 qpu = AnyonYukonQPU(host = host, user = user, access_token = token, project_id = project_id)
 
