9d427538a7398783485c809d641b40a162d3fc93
Alireza Najafi-Yazdi <alireza@anyonsys.com>
2024-01-25 13:59:18 -0500

    fix: project_id

--- tutorials/single_qubit_readout_fidelity.jl
+++ tutorials/single_qubit_readout_fidelity.jl
@@ -8,6 +8,7 @@ user = ENV["THUNDERHEAD_USER"]
 token = ENV["THUNDERHEAD_TOKEN"]
 host = ENV["THUNDERHEAD_HOST"]
 project_id = ENV["THUNDERHEAD_PROJECT_ID"]
+@info "Submitting job to $(project_id)"
 
 qpu = AnyonYukonQPU(host = host, user = user, access_token = token, project_id = project_id)
 
