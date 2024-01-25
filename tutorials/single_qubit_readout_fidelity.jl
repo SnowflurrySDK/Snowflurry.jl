7438f0661ab9debbdbd9e4f1888bd2a539f97f14
Alireza Najafi-Yazdi <alireza@anyonsys.com>
2024-01-25 14:07:23 -0500

    fix: env secrets

--- tutorials/single_qubit_readout_fidelity.jl
+++ tutorials/single_qubit_readout_fidelity.jl
@@ -5,10 +5,9 @@ circuit = QuantumCircuit(qubit_count = 1, name = "fidelity")
 push!(circuit, readout(1, 1))
 
 user = ENV["THUNDERHEAD_USER"]
-token = ENV["THUNDERHEAD_TOKEN"]
+token = ENV["THUNDERHEAD_API_TOKEN"]
 host = ENV["THUNDERHEAD_HOST"]
 project_id = ENV["THUNDERHEAD_PROJECT_ID"]
-@info "Submitting job to $(project_id)"
 
 qpu = AnyonYukonQPU(host = host, user = user, access_token = token, project_id = project_id)
 
