93dd4a173eec0fbfa329cf6c80b22a86ec49ef8d
Facundo Sosa-Rey <facundo@anyonsys.com>
2024-02-07 15:44:55 -0500

    feat: add realm to tutorials and CI secrets

--- tutorials/asynchronous_jobs.jl
+++ tutorials/asynchronous_jobs.jl
@@ -9,9 +9,9 @@ user = ENV["THUNDERHEAD_USER"]
 token = ENV["THUNDERHEAD_API_TOKEN"]
 host = ENV["THUNDERHEAD_HOST"]
 project_id = ENV["THUNDERHEAD_PROJECT_ID"]
+realm = ENV["THUNDERHEAD_REALM"]
 
-
-qpu = AnyonYukonQPU(host = host, user = user, access_token = token, project_id = project_id)
+qpu = AnyonYukonQPU(host = host, user = user, access_token = token, project_id = project_id, realm = realm)
 
 shot_count = 200
 task = Task(() -> transpile_and_run_job(qpu, circuit, shot_count))
