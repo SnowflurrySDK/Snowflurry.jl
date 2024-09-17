b0820a2c6093ef88b34a84d8b33edaa686b3fe2b
Nicolas Poirier <nicolas@anyonsys.com>
2024-09-17 11:11:38 -0400

    docs: add update_circuit_qubit_count

--- src/core/quantum_circuit.jl
+++ src/core/quantum_circuit.jl
@@ -81,6 +81,37 @@ Base.@kwdef struct QuantumCircuit
     end
 end
 
+"""
+    update_circuit_qubit_count(
+        quantum_cicuit::QuantumCircuit,
+        qubit_count::Int,
+    )::QuantumCircuit
+
+Updates the `qubit_count` of the `quantum_circuit`.
+
+# Examples
+```jldoctest
+julia> circuit = QuantumCircuit(qubit_count = 1, instructions = [sigma_x(1)])
+Quantum Circuit Object:
+   qubit_count: 1 
+   bit_count: 1 
+q[1]:──X──
+          
+
+
+
+julia> larger_circuit = update_circuit_qubit_count(circuit, 2)
+Quantum Circuit Object:
+   qubit_count: 2 
+   bit_count: 1 
+q[1]:──X──
+          
+q[2]:─────
+          
+
+
+```
+"""
 function update_circuit_qubit_count(
     quantum_cicuit::QuantumCircuit,
     qubit_count::Int,
